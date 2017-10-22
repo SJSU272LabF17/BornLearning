fb49a1928a455dcb3006b77e683ca9327dd32f11
nivram07 <marvinb_flores@yahoo.com>
2017-10-22 15:27:47 -0700

    Create base application

---
+++ data/src/main/java/com/grp15/cmpe272/unitedwayapp/data/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.grp15.cmpe272.unitedwayapp.data
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
