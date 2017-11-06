80b8183c02901d6f70ab14161c988ffbb001bbd2
Marvin Flores <marvinbflores0000@gmail.com>
2017-11-05 16:58:12 -0800

    remove unused code

--- bornlearning/src/main/java/com/grp15/cmpe272/unitedwayapp/bornlearning/MainActivity.kt
+++ bornlearning/src/main/java/com/grp15/cmpe272/unitedwayapp/bornlearning/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.grp15.cmpe272.unitedwayapp.bornlearning
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 /**
  * This class handles the main page of the application. This will include:
@@ -16,10 +14,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun openSchoolReadiness(view : View) : Boolean {
-//        val intent = Intent(this,
-//                com.pixelconic.schoolreadiness.SchoolReadinessActivity::class.java)
-//        startActivity(intent)
-        return true
-    }
 }
