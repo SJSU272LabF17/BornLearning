076a9626acf237de018ca5825fa1bffc27126a75
Madhur Gor <madhurgor@gmail.com>
2017-11-10 18:49:25 -0800

    Implement Question UI

---
+++ bornlearning/src/main/java/com/grp15/cmpe272/unitedwayapp/bornlearning/profile/QuestionImplementationActivity.kt
@@ -0,0 +1,16 @@
+package com.grp15.cmpe272.unitedwayapp.bornlearning.profile
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.grp15.cmpe272.unitedwayapp.bornlearning.R
+
+
+/**
+ * Created by madhu on 11/9/2017.
+ */
+class QuestionImplementationActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_implementation)
+    }
+}
